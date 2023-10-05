4060035fd955aab245c177830b1bb10ca54ee04e
Rodrigo Astorga <rodrigo.astorgaespinoza@cencosud.cl>
2023-10-05 20:53:54 -0300

    Initial Commit

---
+++ Meli/MeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliApp.swift
+//  Meli
+//
+//  Created by Rodrigo Astorga Espinoza on 05-10-23.
+//
+
+import SwiftUI
+
+@main
+struct MeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
