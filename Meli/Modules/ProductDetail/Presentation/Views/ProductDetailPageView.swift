fc01a4eb9149f0540d4f98c23637a5e77f341087
Rodrigo Astorga <rodrigo.astorgaespinoza@cencosud.cl>
2023-10-07 17:19:50 -0300

    Create `ProductDetailPageView` file

---
+++ Meli/Modules/ProductDetail/Presentation/Views/ProductDetailPageView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailPageView.swift
+//  Meli
+//
+//  Created by Rodrigo Astorga Espinoza on 07-10-23.
+//
+
+import SwiftUI
+
+struct ProductDetailPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailPageView()
+}
