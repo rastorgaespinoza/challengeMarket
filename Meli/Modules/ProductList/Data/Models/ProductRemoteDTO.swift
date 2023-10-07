4006be98c75fde50bda397dca905e1e1b894e7f5
Rodrigo Astorga <rodrigo.astorgaespinoza@cencosud.cl>
2023-10-07 15:07:02 -0300

    Create `ProductListPageView` and `ProductRemoteDTO`

---
+++ Meli/Modules/ProductList/Data/Models/ProductRemoteDTO.swift
@@ -0,0 +1,8 @@
+//
+//  ProductRemoteDTO.swift
+//  Meli
+//
+//  Created by Rodrigo Astorga Espinoza on 07-10-23.
+//
+
+import Foundation
---
+++ Meli/Modules/ProductList/Presentation/ProductListPageView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductListPageView.swift
+//  Meli
+//
+//  Created by Rodrigo Astorga Espinoza on 07-10-23.
+//
+
+import SwiftUI
+
+struct ProductListPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductListPageView()
+}
