81fa6f48169488e6b63f56fa36ce0dab97813b83
Rodrigo Astorga <rodrigo.astorgaespinoza@cencosud.cl>
2023-10-08 22:05:28 -0300

    Add fonts Proxima Nova with utils methods

---
+++ Meli/Supporting Files/Fonts/ProximaNovaFont.swift
@@ -0,0 +1,18 @@
+//
+//  InterFont.swift
+//  Meli
+//
+//  Created by Rodrigo Astorga Espinoza on 08-10-23.
+//
+
+enum InterFont: String {
+  case regular = "Inter-Regular"
+  case medium = "Inter-Medium"
+}
+
+enum ProximaNovaFont: String {
+  case regular = "ProximaNova-Regular"
+  case semibold = "ProximaNova-Semibold"
+  case thin = "ProximaNova-Thin"
+  case light = "ProximaNova-Light"
+}
